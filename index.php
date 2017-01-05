5aa309947ef8da77fc922e43c6791907eb87125d
lealmbrt <lilliedrosera@gmail.com>
2017-01-05 17:25:42 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	
+	<meta charset="utf-8">
+	<meta property="og:url"           content="http://www.your-domain.com/your-page.html" /><!-- à modifier-->
+	<meta property="og:type"          content="website" /><!-- à modifier -->
+	<meta property="og:title"         content="Capsule Transfer" /><!-- à modifier -->
+	<meta property="og:description"   content="Share your file, with fun !" /><!-- à modifier -->
+	<meta property="og:image"         content="http://www.your-domain.com/path/image.jpg" /><!-- à modifier -->
+	
+	<title>Capsule Transfer</title>
+  
+	<link rel="stylesheet" type="text/css" href="assets/font/fonts.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/styleBtn.css">
+  
+
+
+
+
+  </head>
+
+
+
+   
+<body onload="ftnDoor();">
+	
+    <main>
+
+		<!-- capsule -->      
+        <svg version="1.1" id="homePageCapsule" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+        y="0px" width="100%" height="100%" viewBox="0 0 3840 2160" enable-background="new 0 0 3840 2160" xml:space="preserve">
+	        <g>
+		        <g>
+		        <path fill="#E6E6E6" d="M1727.053,834.04c-1.678,2.178-3.052,6.21-3.052,8.96v720c0,2.75,1.374,6.782,3.052,8.96l98.13,127.314
+		            c1.68,2.178,5.303,3.96,8.053,3.96l173.529,0.001c2.75,0,6.374-1.782,8.053-3.96l98.129-127.314
+		            c1.679-2.178,3.053-6.209,3.053-8.959V843c0-2.75-1.374-6.781-3.053-8.96l-98.129-127.313c-1.679-2.178-5.303-3.961-8.053-3.961
+		            h-173.529c-2.75,0-6.373,1.783-8.053,3.961L1727.053,834.04z"/>
+		        </g>
+		        <g>
+		        <path fill="#019DE7" d="M2121.841,1568.001c2.75,0,3.58,1.746,1.845,3.878l-103.689,127.477c-1.735,2.134-5.405,3.879-8.155,3.879
+		            l-183.682-0.001c-2.75,0-6.42-1.745-8.156-3.879l-103.689-127.476c-1.735-2.134-0.905-3.879,1.845-3.879L2121.841,1568.001z"/>
+		        </g>
+		        <g>
+		        <path fill="#332E2D" d="M2127.095,1541.249c2.75,0,5,2.25,5,5v18.752c0,2.75-2.25,5-5,5L1712.905,1570c-2.75,0-5-2.25-5-5v-18.752
+		            c0-2.75,2.25-5,5-5L2127.095,1541.249z"/>
+		        </g>
+		        <g>
+		        <path fill="#019DE7" d="M1718.159,838c-2.75,0-3.58-1.746-1.845-3.879l103.689-127.477c1.736-2.131,5.406-3.878,8.156-3.878
+		            h183.682c2.75,0,6.42,1.747,8.155,3.878l103.689,127.477c1.735,2.135,0.905,3.879-1.845,3.879H1718.159z"/>
+		        </g>
+		        <g opacity="0.2">
+		        <path d="M1923.083,707.766c0-2.75,2.25-5,5-5h86.841c2.75,0,6.42,1.747,8.155,3.878l103.689,127.477
+		            c1.735,2.133,0.905,3.879-1.845,3.879h-196.841c-2.75,0-5-2.25-5-5V707.766z"/>
+		        </g>
+		        <g opacity="0.2">
+		        <path d="M1919.999,1698.234c0,2.75,2.25,5,5,5l86.841,0.001c2.75,0,6.42-1.745,8.155-3.879l103.689-127.477
+		            c1.735-2.133,0.905-3.878-1.845-3.878L1924.999,1568c-2.75,0-5,2.25-5,5V1698.234z"/>
+		        </g>
+		        <g>
+		        <path fill="#332E2D" d="M1712.905,864.752c-2.75,0-5-2.25-5-5V841c0-2.75,2.25-5,5-5h414.189c2.75,0,5,2.25,5,5v18.752
+		            c0,2.75-2.25,5-5,5H1712.905z"/>
+		        </g>
+		        <rect x="1923.083" y="864.751" opacity="0.1" width="192.916" height="676.497"/>
+		        <path id="capsuleOpening" fill="#333333" d="M1724.001,940l116.998,0.001c5.5,0,10,4.5,10,10v508c0,5.5-4.5,10-10,10h-116.998V940z
+		        "/>
+		        <g id="capsuleDoor" class="">
+		        <rect id="capsuleDoor1" x="1557.002" y="922.311" fill="#E6E6E6" width="166.998" height="564.379"/>
+		        <rect id="capsuleDoor2" x="1724.001" y="922.311" fill="none" width="166.998" height="564.379"/>
+		        </g>
+	        </g>
+        </svg>
+
+		<!-- tube -->        
+        <svg version="1.1" id="homePageTube" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+        y="0px" width="100%" height="100%" viewBox="0 0 3840 2160" enable-background="new 0 0 3840 2160" xml:space="preserve">
+	        <g>
+	        <g>
+	        <path fill="#666666" d="M8781.646,961.516v482.97H2739.997v-241.49h210c141,0,263-174.89,277-241.48h757.34
+	            c14,66.59,136,241.48,277,241.48h4515.34v-241.48H8781.646z"/>
+	        <rect x="2740" y="1299.458" opacity="0.2" width="6036.681" height="145.028"/>
+	        <path opacity="0.5" fill="#FFFFFF" d="M8776.681,959.26v241.48H4261.342c-141,0-263-174.891-277-241.48H8776.681z"/>
+	        </g>
+	        <g>
+	        <circle fill="#B3B3B3" cx="3785.673" cy="1408.231" r="36.254"/>
+	        <path opacity="0.2" d="M3821.927,1408.23c0,20.021-16.231,36.254-36.255,36.254c-20.022,0-36.254-16.232-36.254-36.254"/>
+	        <circle fill="#B3B3B3" cx="3785.673" cy="997.77" r="36.254"/>
+	        <circle fill="#B3B3B3" cx="3785.673" cy="1134.591" r="36.254"/>
+	        <circle fill="#B3B3B3" cx="3785.673" cy="1271.411" r="36.254"/>
+	        <path opacity="0.2" d="M3821.927,1271.411c0,20.021-16.231,36.254-36.255,36.254c-20.022,0-36.254-16.232-36.254-36.254"/>
+	        <path opacity="0.2" d="M3821.927,1134.591c0,20.021-16.231,36.254-36.255,36.254c-20.022,0-36.254-16.232-36.254-36.254"/>
+	        <path opacity="0.2" d="M3821.927,997.77c0,20.022-16.231,36.254-36.255,36.254c-20.022,0-36.254-16.232-36.254-36.254"/>
+	        <rect x="3769.605" y="917.421" fill="#333333" width="71.995" height="571.16"/>
+	        <rect x="3769.605" y="1271.411" opacity="0.2" width="73.661" height="217.17"/>
+	        <g>
+	            <rect x="3841.6" y="961.516" fill="#B3B3B3" width="18.652" height="72.158"/>
+	            <rect x="3841.6" y="985.676" opacity="0.2" width="18.652" height="24.16"/>
+	            <rect x="3841.6" y="1009.836" opacity="0.3" width="18.652" height="23.839"/>
+	        </g>
+	        <g>
+	            <rect x="3841.6" y="1098.512" fill="#B3B3B3" width="18.652" height="72.158"/>
+	            <rect x="3841.6" y="1122.672" opacity="0.2" width="18.652" height="24.16"/>
+	            <rect x="3841.6" y="1146.832" opacity="0.3" width="18.652" height="23.839"/>
+	        </g>
+	        <g>
+	            <rect x="3841.6" y="1235.33" fill="#B3B3B3" width="18.652" height="72.158"/>
+	            <rect x="3841.6" y="1259.49" opacity="0.2" width="18.652" height="24.16"/>
+	            <rect x="3841.6" y="1283.65" opacity="0.3" width="18.652" height="23.839"/>
+	        </g>
+	        <g>
+	            <rect x="3841.6" y="1372.152" fill="#B3B3B3" width="18.652" height="72.158"/>
+	            <rect x="3841.6" y="1396.311" opacity="0.2" width="18.652" height="24.161"/>
+	            <rect x="3841.6" y="1420.472" opacity="0.3" width="18.652" height="23.838"/>
+	        </g>
+	        </g>
+	        </g>
+        </svg>
+        
+        
+		<header>
+            <div class="color"></div>
+            <h1>Capsule Transfer</h1>
+            <h2 id="blueText">drag your files inside the capsule or click on it</h2>
+		</header>
+        
+        <section>
+            
+            
+            <aside>
+                <div>
+                    <p>On en chie comme des morses sur ce projet à la con, mais laissez nous au moins triper sur les input text qui ne servent à rien ça sera bien gentil.</p>
+                    <p>A vrai dire on ne sait pas quoi écrire ici alors autant y balourder des conneries, nan ?</p>
+                    <p>Ni! Ni! Ni! Ni! …Are you suggesting that coconuts migrate? Now, look here, my good man. Found them? In Mercia?! The coconut's tropical!</p>
+                </div>
+            </aside>
+            
+        	
+            <form method="post" action="upload.php" enctype="multipart/form-data">
+                
+                <div id="zoneUploadFile" class="">
+                    <!-- label pour input file masqué -->
+                    <label for="btnUpload">
+                        <!-- zone pointillé & cliquable-->
+                        <div id="zoneLoadId" onmouseout="ftnDoor();" onmouseover="ftnDoor();" class="upload"></div>
+                    </label>
+
+
+                    <!-- On limite le fichier à 100Ko -->
+                    <input type="hidden" name="MAX_FILE_SIZE" value="100000">
+
+
+                    <!-- input file fichierUp > passe le nom du fichier choisi -->
+                    <input name="fichierUp" id="btnUpload" type="file" class="hide" onchange="masqueZone('zoneLoadId','btnUpload');">
+
+                    <!-- bouton ENVOI -->
+                    <input id="btnShootId" onclick="ftnUpload();" class="btnClassic opacity" type="button" name="uploader" value="Yup, shoot !">
+                </div>
+                
+
+                <!--  -->
+                <div id="zoneEmail" class="inputMail hide">
+                    <p>E-mail of your contact :</p>
+                    <input name="mailDestinataire" id="contactMail" class="inputText" type="email">
+                    <p>Your e-mail :</p>
+                    <input name="mailExpe" class="inputText" type="email">
+                    <input id="btnSendId" class="btnClassic" type="submit" name="envoyer" value="Send there !">
+                </div>
+                
+            </form>
+            
+			
+		</section>
+        
+        
+		
+		<footer>
+
+		</footer>
+	</main>
+    
+    <script>
+    
+        function ftnDoor() 
+            { 
+                document.getElementById("capsuleDoor").classList.toggle("closeDoor");
+            }
+        
+        
+        function toggleHide(elemId)
+            {
+                //affiche l'element dont l'Id est passée en param
+                document.getElementById(elemId).classList.toggle("hide");
+            }
+        
+        function toggleOpacity(elemId)
+            {
+                //affiche l'element dont l'Id est passée en param
+                document.getElementById(elemId).classList.toggle("opacity");
+            }
+
+        
+        function masqueZone(zoneDashed, inputId)
+            {
+            //verif si inputId est rempli. 
+            //Si oui > masque zone pointillé et affiche btn.
+            //Si non > rien
+
+
+            var choosenFile = document.getElementById(inputId).value;
+
+            if(choosenFile != "") 
+                {
+                	//supprime pointillé et affiche bouton                   
+                    toggleOpacity('btnShootId');
+                    document.getElementById("zoneLoadId").classList.add("borderUpLoad");
+                    document.getElementById("blueText").innerHTML="all good ?";
+                }
+            else 
+                {
+                    //masque bouton et on affiche pointillé
+                    document.getElementById("zoneLoadId").classList.remove("borderUpLoad");
+                    document.getElementById("btnShootId").classList.add("opacity");
+                    document.getElementById("blueText").innerHTML="drag your files inside the capsule or click on it";
+                }
+
+            }
+        
+        function ftnUpload()
+        {
+            toggleHide('zoneUploadFile');
+            toggleHide('zoneEmail');
+            document.getElementById("blueText").innerHTML="Where should we send it ?";
+        }
+            
+  
+    </script>
+</body>
+</html>
